bff25cf3a0a75b74b2f25ee7be8318e56c287b93
nikkibytes <nicholletteacosta@gmail.com>
2018-05-28 02:53:44 +0000

    made template connect, working mysql database, and added weschools template

--- blog/aboutme/views.py
+++ blog/aboutme/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
+def index(request):
+    return render(request, 'aboutme.html')
+#    template = loader.get_template('aboutme.html')
+#    return HttpResponse(template.render(request, template, context=None))
 # Create your views here.
