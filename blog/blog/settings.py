6b8868992992a89e949201f22a8910bbcb7219a9
nikkibytes <nicholletteacosta@gmail.com>
2018-05-30 03:44:13 +0000

    added myblog

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'aboutme'
+    'aboutme',
+    'myblog',
 ]
 
 MIDDLEWARE = [
