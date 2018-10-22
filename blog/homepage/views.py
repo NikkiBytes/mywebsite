20a0642bf7e4084eaed3b313b0558d1773a185db
NikkiBytes <nicholletteacosta@gmail.com>
2018-10-22 18:35:07 -0400

    added

--- blog/homepage/views.py
+++ blog/homepage/views.py
@@ -7,11 +7,3 @@ from django.views.generic import TemplateView, RedirectView
 
 class HomeView(TemplateView):
     template_name = 'modern_design.html'
-class AboutMeView(TemplateView):
-    template_name = 'aboutme.html'
-class LabsView(TemplateView):
-    template_name = "labview.html"
-class BlogView(TemplateView):
-    template_name = "blogview.html"
-class ContactView(TemplateView):
-    template_name = "contact.html"
